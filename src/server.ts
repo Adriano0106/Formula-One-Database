86e71e94f1f38d3c29a8c8d8c7fdeb37a18a8ecf
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2024-08-30 00:37:28 -0300

    Setup de CORS permitidos

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import cors from 'cors';
 import driverChampionsRoutes from './routes/driversChampions';
 import teamsChampionsRoutes from './routes/teamsChampions';
 import driversRoutes from './routes/drivers';
 
 const app = express();
 app.use(express.json());
+
+// Configuração do CORS
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://adriano-formulaonenext.netlify.app'
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use('/api/champions/drivers', driverChampionsRoutes);
 app.use('/api/champions/teams', teamsChampionsRoutes);
